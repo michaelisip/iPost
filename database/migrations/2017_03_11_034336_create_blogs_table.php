9f30474eb6a2ea993c7f3e61c072bc69baa75dda
Michael John Isip <mj.isip123@gmail.com>
2017-03-22 12:22:19 +0800

    Add files via upload

---
+++ database/migrations/2017_03_11_034336_create_blogs_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBlogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('blogger_id')->unsigned();
+            $table->string('subject');
+            $table->string('body');
+            $table->timestamps();
+
+            $table->foreign('blogger_id')
+                    ->references('id')
+                    ->on('bloggers')
+                    ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blogs');
+    }
+}
